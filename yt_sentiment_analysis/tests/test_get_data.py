b7144cea04ffd3f3edb4a536e88d7288e5b36776
Maja <majam1616@gmail.com>
2022-05-15 19:32:47 -0400

    added tests, moved tests from test_src

--- yt_sentiment_analysis/tests/test_get_data.py
+++ yt_sentiment_analysis/tests/test_get_data.py
@@ -1,15 +1,50 @@
 import unittest
 import json
-
+from unittest.mock import patch
 from yt_sentiment_analysis.constants import DATA_DIR
-
-# from yt_sentiment_analysis.utils.dynamo_db import Dynamo
+from yt_sentiment_analysis.src.get_data import extract_video_details, get_transcripts
+from yt_sentiment_analysis.src.get_data import get_top_videos_by_category
 from yt_sentiment_analysis.utils.get_logger import get_logger
-from datetime import datetime
-
-# from moto import mock_dynamodb
 
 logger = get_logger(__name__)
+
+
+def mocked_requests_get_youtuberesp(*args, **kwargs):
+    """
+    This method will be used by the mock to replace requests.get
+    https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response
+    """
+    class MockResponse:
+        print("ARGS")
+        print(args)
+        print("KWARS")
+        print(kwargs)
+
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    params = kwargs['params']
+    # Mock out all the YT API Responses expected
+    # No params passed (get all videoCategoryId)
+    if args[0] == 'https://youtube.googleapis.com/youtube/v3/videos':
+        if 'videoCategoryId' in params:
+            datapath = DATA_DIR / 'test_data' / 'ytapiresp_get_top_videos_by_category.json'
+            with open(datapath, 'r') as fp:
+                rsp = json.load(fp)
+                return MockResponse(rsp, 200)
+        else:
+            datapath = DATA_DIR / 'test_data' / 'ytapiresp_wo_cat.json'
+            with open(datapath, 'r') as fp:
+                rsp = json.load(fp)
+                return MockResponse(rsp, 200)
+    # elif args[0] == 'http://someotherurl.com/anothertest.json':
+    #     return MockResponse({"key2": "value2"}, 200)
+
+    return MockResponse(None, 404)
 
 
 class TestGetData(unittest.TestCase):
@@ -19,36 +54,64 @@ class TestGetData(unittest.TestCase):
             DATA_DIR / "test_data" / "ytapiresp_get_top_videos_by_category.json"
         ) as fp:
             rsp = json.load(fp)
+            self.full_videos_by_category_data = rsp
             self.videos_by_category_data = rsp["items"]
-        # with open(DATA_DIR / "dynamodb" / "table_schema.json") as sch:
-        #     self.table_schema = json.load(sch)["Table"]
-        # self.client = Dynamo()
 
         # transcripts formatted for a few videos
+        self.video_ids = ['EwTZ2xpQwpA', 'Ka4coAT3YQ4']
         transcript_path = (
             DATA_DIR / "test_data" / "transcripts_EwTZ2xpQwpA_Ka4coAT3YQ4.json"
         )
         with open(transcript_path, "r") as f:
             self.transcript_data = json.load(f)
 
-        # self.client.dynamodb.create_table(
-        #     TableName=self.client.tablename,
-        #     KeySchema=self.table_schema["KeySchema"],
-        #     AttributeDefinitions=self.table_schema["AttributeDefinitions"],
-        #     ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
-        # )
+    @patch('requests.get', side_effect=mocked_requests_get_youtuberesp)
+    def test_get_top_videos_by_category__with_category_id(self, mock_get):
+        """ videos from specific `category_id` """
+        json_data = get_top_videos_by_category(category_id='1')
+        # the mocked yt api response
+        self.assertEqual(json_data, self.full_videos_by_category_data)
 
-        # maek sure table exists
-        # assert self.client.get_table().table_status == "ACTIVE"
-
-    def test_get_top_videos_by_category(self):
-        pass
+    @patch('requests.get', side_effect=mocked_requests_get_youtuberesp)
+    def test_get_top_videos_by_category__bad_category_id(self, mock_get):
+        """ `category_id` not valid """
+        with self.assertRaises(AssertionError):
+            get_top_videos_by_category(category_id='9171')
 
     def test_get_transcripts(self):
-        pass
+        transcripts = get_transcripts(self.video_ids)
+        raise NotImplementedError
+
+    @patch('requests.get', side_effect=mocked_requests_get_youtuberesp)
+    def test_get_top_videos_by_category__none_category_id(self, mock_get):
+        json_data = get_top_videos_by_category()
+        # the mocked yt api response
+        self.assertEqual(json_data, self.full_videos_by_category_data)
 
     def test_extract_video_details(self):
-        pass
+        # expected input obj
+        extracts = extract_video_details(self.videos_by_category_data)
+        print(extracts)
+        # make sure all extracted
+        self.assertEqual(len(extracts), len(self.videos_by_category_data))
+        # make sure all details extracted
+        test_in = self.videos_by_category_data[0]
+        test_out = extracts[0]
+        print('test_in')
+        print(test_in)
+        print('test_out')
+        print(test_out)
+        # make sure all keys are in output
+        self.assertTrue('video_id' in test_out)
+        self.assertTrue('title' in test_out)
+        self.assertTrue('description' in test_out)
+        self.assertTrue('channel_id' in test_out)
+        self.assertTrue('channel_name' in test_out)
+        self.assertTrue('tags' in test_out)
+        self.assertTrue('statistics' in test_out)
+        self.assertTrue('category_id' in test_out)
+        self.assertTrue('published_dt' in test_out)
+        self.assertTrue('insert_dt' in test_out)
 
 
 if __name__ == "__main__":
