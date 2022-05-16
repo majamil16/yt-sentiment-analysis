5d09cedbc7567f781b8f463a455df1723d301446
Maja <majam1616@gmail.com>
2022-05-16 01:22:10 -0400

    linting; added proper response to get_transcripts

--- yt_sentiment_analysis/src/get_data.py
+++ yt_sentiment_analysis/src/get_data.py
@@ -3,31 +3,33 @@ from functools import wraps
 import requests
 from pathlib import Path
 import time
-from yt_sentiment_analysis.constants  import YOUTUBE_API_KEY, CATEGORIES, DATA_DIR 
+from yt_sentiment_analysis.constants import YOUTUBE_API_KEY, CATEGORIES, DATA_DIR
 import json
-from youtube_transcript_api  import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import Formatter
-import time
 from datetime import datetime
 
-from yt_sentiment_analysis.utils.dynamo_db import Dynamo
+# from yt_sentiment_analysis.utils.dynamo_db import Dynamo
 
 from yt_sentiment_analysis.utils.get_logger import get_logger
 logger = get_logger(__name__)
 
+
 class MyCustomFormatter(Formatter):
 
-    def format_transcript(self, video_id:str, transcript, **kwargs):
+    def format_transcript(self, video_id: str, transcript, **kwargs):
         # Do your custom work in here, but return a string.
-        fmt = {video_id : transcript}
+        fmt = {video_id: transcript}
         return self.format_transcripts([fmt])
 
     def format_transcripts(self, transcripts, **kwargs):
-        # Do your custom work in here to format a list of transcripts, but return a string.
+        print('in my formatter')
+        # Do your custom work in here to format a list of transcripts,
+        # but return a string.
         transcripts_fmt = []
         # transcripts_fmt = {}
         print(transcripts)
-        for id, transcript in transcripts.items(): 
+        for id, transcript in transcripts.items():
             transcript_fmt = {}
             print('id' + id)
             print(transcript)
@@ -42,32 +44,39 @@ class MyCustomFormatter(Formatter):
                 transcript_fmt['transcript'] = joined_tr
                 # append the single transcript dict to the list of transcripts
                 transcripts_fmt.append(transcript_fmt)
+        if kwargs.get('as_list'):
+            t0_type = type(transcripts_fmt[0])
+            logger.debug(f'format_transcripts - as_list=True {t0_type}')
+            return transcripts_fmt
         return json.dumps(transcripts_fmt)
 
 
-def get_top_videos_by_category(category_id:str=None, save:bool=False, max_results:int=50) -> dict:
+def get_top_videos_by_category(category_id: str = None,
+                               save: bool = False,
+                               max_results: int = 50) -> dict:
     """
     Get the top videos for the specified `category_id`.
 
-    Category IDs for US categories are stored in constants.py CATEGORIES variable. Note - some return no response.
+    Category IDs for US categories are stored in constants.py CATEGORIES variable.
+    Note - some return no response.
 
     Params
     ------
-    : category_id: YouTube API videoCategoryId of videos to get. 
-    : max_results : Max results 
+    : category_id: YouTube API videoCategoryId of videos to get.
+    : max_results : Max results
     """
     if category_id is not None:
-        assert category_id in list(CATEGORIES.keys()) , "enter a valid category_id"
+        assert category_id in list(CATEGORIES.keys()), "enter a valid category_id"
     url = 'https://youtube.googleapis.com/youtube/v3/videos'
     params = {
-    'part': ['snippet','contentDetails','statistics'],
-    "chart" : "mostPopular",
-    "regionCode": "US",
-    "key" : YOUTUBE_API_KEY,
-    "maxResults" : max_results,
+            'part': ['snippet', 'contentDetails', 'statistics'],
+            "chart": "mostPopular",
+            "regionCode": "US",
+            "key": YOUTUBE_API_KEY,
+            "maxResults": max_results,
     }
     # add category_id if it is passed in
-    if category_id : 
+    if category_id:
         params["videoCategoryId"] = category_id
 
     # get the top videos for the category
@@ -76,23 +85,25 @@ def get_top_videos_by_category(category_id:str=None, save:bool=False, max_result
         data = r.json()
         print("JSON KEYS")
         if save:
-            if category_id : 
+            if category_id:
                 fname = 'ytapiresp_get_top_videos_by_category_{category_id}.json'
                 with open(Path(DATA_DIR)/fname, 'w') as fp:
                     json.dump(data, fp)[[]]
-            else : 
+            else:
                 fname = 'ytapiresp_get_top_videos_by_category.json'
             with open(Path(DATA_DIR)/fname, 'w') as fp:
                 json.dump(data, fp)
         return data
-    else : 
+    else:
         logger.error("boo!")
         print(r.status_code)
         print(r.text)
         return None
 
 
-def get_transcripts(video_ids:list, load_filepath:str=None, save_filepath:str=None):
+def get_transcripts(video_ids: list,
+                    load_filepath: str = None,
+                    save_filepath: str = None, **kwargs):
     """
     Get the transcripts for `video_ids` list
 
@@ -102,9 +113,15 @@ def get_transcripts(video_ids:list, load_filepath:str=None, save_filepath:str=No
     :load_filepath (opt): file path to load data from instead.
     :save_filepath (opt): file path to save data to.
 
+    Returns:
+    --------
+    :json_formatted
+
     """
+    print('KWSRGS =')
+    print(kwargs)
     if load_filepath:
-        # If file path is specified, return 
+        # If file path is specified, return
         with open(load_filepath, 'r') as f:
             loaded = json.load(f)
             loaded_ids = loaded.keys()
@@ -114,12 +131,12 @@ def get_transcripts(video_ids:list, load_filepath:str=None, save_filepath:str=No
     formatter = MyCustomFormatter()
 
     # turns the transcript into a JSON string.
-    json_formatted = formatter.format_transcripts(transcripts)
+    json_formatted = formatter.format_transcripts(transcripts, kwargs=kwargs)
     # Now we can write it out to a file.
     if save_filepath is not None:
         with open(save_filepath, 'w', encoding='utf-8') as json_file:
             json_file.write(json_formatted)
-    return json
+    return json_formatted, video_ids_not_found
 
 
 def mapper(func):
@@ -133,7 +150,8 @@ def mapper(func):
 
 
 @mapper
-def extract_video_details(video: dict, format_dynamo=True) -> list[dict]:
+def extract_video_details(video: dict,
+                          format_dynamo=True) -> list[dict]:
     """
     Extract relevant info from the /videos api response (youtube#videoListResponse)
 
