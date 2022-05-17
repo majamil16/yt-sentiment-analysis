3ba2e569bbb5c1c0fdac0e7bceef4cf748cf2247
Maja <majam1616@gmail.com>
2022-05-17 01:24:21 -0400

    formatting

--- yt_sentiment_analysis/utils/dynamo_db.py
+++ yt_sentiment_analysis/utils/dynamo_db.py
@@ -12,6 +12,7 @@ from yt_sentiment_analysis.utils.get_logger import get_logger
 
 logger = get_logger(__name__)
 
+
 class Dynamo():
     """
     Simple class to handle creating / dropping dynamodb table as needed
@@ -29,9 +30,8 @@ class Dynamo():
         self.tablename = tablename
         assert self.tablename is not None, 'Initialize `tablename` or env variable `TABLE` must be present.'
 
-    def get_table(self) :
+    def get_table(self):
         return self.dynamodb.Table(self.tablename)
-
 
     def drop_ddb_table(self):
         # table = self.dynamodb.Table(os.getenv('TABLE'))
@@ -39,49 +39,52 @@ class Dynamo():
         table.delete()
 
     def check_table_status(self):
-        try :
-          table = self.dynamodb.Table(self.tablename)
-          status = table.table_status
-          return status
+        try:
+            table = self.dynamodb.Table(self.tablename)
+            status = table.table_status
+            return status
 
-        except self.dynamodb.meta.client.exceptions.ResourceNotFoundException as exc :
-          print(exc)
-          print('Table does not exist')
-          raise exc
+        except self.dynamodb.meta.client.exceptions.ResourceNotFoundException as exc:
+            print(exc)
+            print('Table does not exist')
+            raise exc
 
-    def create_ddb_table(self, attributes:dict, partition_key_nm:str, sort_key_nm=None, rcu=1, wcu=1):
-      """
-      To generate DynamoDB create table API call based on input attributes, partition, sort key.
+    def create_ddb_table(self, attributes: dict, partition_key_nm: str,
+                         sort_key_nm=None, rcu=1, wcu=1):
+        """
+        To generate DynamoDB create table API call based on input attributes,
+        partition, sort key.
 
-      Params:
-      -------
-      :attributes - dict[str, str] where { ATTR_NAME : ATTR_TYPE }
-      :partition_key - required. Partition key for the table
-      :sort_key - optional. Sort (range) key for the table
-      :rcu - read capacity units. default to 1
-      :wcu - write capacity units. default to 1
-      """
-      attrs = [{"AttributeName": k, "AttributeType": v} for k, v in attributes.items()]
-      keys = [{"AttributeName": partition_key_nm, "KeyType": "HASH"} ]
-      if sort_key_nm is not None : 
-        keys.append({"AttributeName": sort_key_nm, "KeyType": "RANGE"})
+        Params:
+        -------
+        :attributes - dict[str, str] where { ATTR_NAME : ATTR_TYPE }
+        :partition_key - required. Partition key for the table
+        :sort_key - optional. Sort (range) key for the table
+        :rcu - read capacity units. default to 1
+        :wcu - write capacity units. default to 1
+        """
+        attrs = [{"AttributeName": k, "AttributeType": v} for k, v in attributes.items()]
+        keys = [{"AttributeName": partition_key_nm, "KeyType": "HASH"}]
+        if sort_key_nm is not None:
+            keys.append({"AttributeName": sort_key_nm, "KeyType": "RANGE"})
 
-      schema_def = {
-          "AttributeDefinitions": attrs,
-          "KeySchema": keys,
-          "ProvisionedThroughput": {"ReadCapacityUnits": rcu, "WriteCapacityUnits": wcu},
-      }  
-                          
-      table = self.dynamodb.create_table(
-        TableName=os.getenv('TABLE'),
-        KeySchema=keys,
-        AttributeDefinitions=attrs,
-        ProvisionedThroughput={"ReadCapacityUnits": rcu, "WriteCapacityUnits": wcu},
-      )
-      return table
+        schema_def = {
+            "AttributeDefinitions": attrs,
+            "KeySchema": keys,
+            "ProvisionedThroughput": {"ReadCapacityUnits": rcu,
+                                      "WriteCapacityUnits": wcu
+                                      },
+        }
+        table = self.dynamodb.create_table(
+            TableName=os.getenv('TABLE'),
+            KeySchema=keys,
+            AttributeDefinitions=attrs,
+            ProvisionedThroughput={"ReadCapacityUnits": rcu, "WriteCapacityUnits": wcu},
+        )
+        return table
 
     @classmethod
-    def format(cls, data:dict) -> dict:
+    def format(cls, data: dict) -> dict:
         """
         format dict as DynamoDB-format json data for DDB insert
 
@@ -96,19 +99,19 @@ class Dynamo():
         print(dynamodb_json)
         return dynamodb_json
 
-    def insert(self, formatted_data:dict):
+    def insert(self, formatted_data: dict):
         """
         DDB insert of formatted_data
 
         Params
         ------
-        formatted_data : 
+        formatted_data :
         """
         tbl = self.get_table()
         resp = tbl.put_item(Item=formatted_data)
         return resp
-    
-    def update(self, formatted_data:dict, src_obj:str):
+
+    def update(self, formatted_data: dict, src_obj: str):
         """
         DDB update existing item based on formatted_data[`video_id`]
 
@@ -119,30 +122,33 @@ class Dynamo():
         """
         partition_key = 'video_id'
         sort_key = None
-        if src_obj not in ['transcript', 'video'] : 
-            logger.error("results: status must be one of %r." % ['transcript', 'video'])
-            raise ValueError("results: status must be one of %r." % ['transcript', 'video'])
+        if src_obj not in ['transcript', 'video']:
+            logger.error("results: status must be one of %r, %r."
+                         % ['transcript', 'video'])
+            raise ValueError("results: status must be one of %r, %r."
+                             % ['transcript', 'video'])
 
         try:
             # update expression will differ depending on src
-            update_exp_map = ','.join([f'{k}=:{k}' for k in formatted_data.keys() if k not in [partition_key, sort_key] ])
+            update_exp_map = ','.join([f'{k}=:{k}' for k in formatted_data.keys()
+                                      if k not in [partition_key, sort_key]])
             update_exp_map = f"set {update_exp_map}"
-            logger.debug(f"Update expression : ")
+            logger.debug("Update expression : ")
             logger.debug(update_exp_map)
 
             # exp_att_map = ','.join([f':{k}={v}' for k, v in formatted_data.items()])
 
-            exp_att_map = { f':{k}' : v for k, v in formatted_data.items() if k not in [partition_key, sort_key]  }
-
+            exp_att_map = {f':{k}': v for k, v in formatted_data.items()
+                           if k not in [partition_key, sort_key]}
 
             # exp_att_map = f"set {exp_att_map}"
-            logger.debug(f"Expression Attribute Map : ")
+            logger.debug("Expression Attribute Map:")
             logger.debug(exp_att_map)
 
             response = self.get_table().update_item(
                 Key={'video_id': formatted_data['video_id']},
-                UpdateExpression=update_exp_map ,
-                ExpressionAttributeValues= exp_att_map,
+                UpdateExpression=update_exp_map,
+                ExpressionAttributeValues=exp_att_map,
                 ReturnValues="UPDATED_NEW")
         except ClientError as err:
             logger.error(
@@ -153,17 +159,17 @@ class Dynamo():
         else:
             return response['Attributes']
 
-    def upsert(self, formatted_data:dict, src_obj:str):
+    def upsert(self, formatted_data: dict, src_obj: str):
         """
         Update or insert.
         """
-        ## try get_item  
-        item = self.get_table().get_item(Key={'video_id':formatted_data['video_id']})
+        # try get_item
+        item = self.get_table().get_item(Key={'video_id': formatted_data['video_id']})
         # if found, update
         if 'Item' in item:
             return self.update(formatted_data, src_obj)
         # else insert
-        else :
+        else:
             return self.insert(formatted_data)
 
 
@@ -177,18 +183,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     client = Dynamo()
-    if len(sys.argv) != 2: # we want 2 args (the 2nd is the flag)
+    if len(sys.argv) != 2:  # we want 2 args (the 2nd is the flag)
         print('Flags must be exactly ONE OF : --create, --drop, --check')
-        
     elif args.create and args.drop:
         print('both')
     elif args.create:
-        attributes = { "video_id" : "S", "transcript" : "S", "category_id":"S", "category_nm" : "S"}
+        attributes = {"video_id": "S", "transcript": "S", "category_id": "S", "category_nm": "S"}
         dynamo_table = client.create_ddb_table(attributes=attributes, partition_key_nm='video_id')
         print("Table status:", dynamo_table.table_status)
     elif args.drop:
         client.drop_ddb_table()
-        print("Table status: dropped" )
+        print("Table status: dropped")
     elif args.check:
         status = client.check_table_status()
         print(status)
